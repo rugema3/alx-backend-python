d0911719327159c012d3fedfacdee51c3bccc536
rugema3 <rugema61@gmail.com>
2024-01-11 17:25:18 +0200

    adding docstings to 4-define_variables module.

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""The 4-define_variables module defines variables with their types."""
 a: int = 1
 pi: float = 3.14
 i_understand_annotations: bool = True
---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""7-to_kv module."""
+
+
+def to_kv():
+    """Define to-kv function.
+    Description:
+
+    """
+    return None
