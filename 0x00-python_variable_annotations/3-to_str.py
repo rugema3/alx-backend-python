b0b4a1f3b361bffae75fd8c8c3b661a972a8b53b
rugema3 <rugema61@gmail.com>
2024-01-11 10:59:01 +0200

    a type-annotated function that takes a float argument and returns a string.

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""A module handling the conversion of float to string."""
+
+
+def to_str(n: float) -> str:
+    """Define to_string function.
+    Description:
+                The function converts a float to a string.
+    """
+    return str(n)
