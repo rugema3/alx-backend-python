c33403394f051df2a44d04ad3ca6d2f9b4a79259
rugema3 <rugema61@gmail.com>
2024-01-11 18:27:57 +0200

    a type-annotated function to_kv that takes a string k and an int OR float v as arguments and returns a tuple.

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 """7-to_kv module."""
+from typing import Tuple, Union
 
 
-def to_kv():
-    """Define to-kv function.
-    Description:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Converts a string-key and int/float-value pair to a tuple.
 
+    Args:
+        k (str): The input string key.
+        v (Union[int, float]): The input int or float value.
+
+    Returns:
+        Tuple[str, float]: A tuple containing the string key and
+        the square of the int/float value.
     """
-    return None
+    result: float = float(v ** 2)
+    return k, result
