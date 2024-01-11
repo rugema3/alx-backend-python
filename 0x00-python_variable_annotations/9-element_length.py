e747e1fcb895da81535bb9b777843d6b8a8c14cc
rugema3 <rugema61@gmail.com>
2024-01-11 18:39:14 +0200

    annotating the function and its given return values

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from typing import List, Tuple
+
+
+def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+    """Calculate the length of each element in the input list.
+
+    Args:
+        lst (List[str]): The input list of strings.
+
+    Returns:
+        List[Tuple[str, int]]:  A list of tuples, each containing a string
+                                from the input list
+                            and its corresponding length.
+    """
+    return [(i, len(i)) for i in lst]
