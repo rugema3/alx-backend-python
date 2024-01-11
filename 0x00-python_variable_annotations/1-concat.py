08da37d27542912e617405c4df6ca603962eeb44
rugema3 <rugema61@gmail.com>
2024-01-11 10:34:42 +0200

    a type-annotated function concat that concatenates two strings.

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""A module that contains  a type-annotated function concat."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Define concat type-annotated function.
+    Description:
+                The function takes arguments str1 and str2 as strings
+                and returns the concantanated string.
+    """
+    return str1 + str2
