961d2044bba32cf0927f5ca3e600bf3b332e0870
rugema3 <rugema61@gmail.com>
2024-01-15 19:47:26 +0200

    added annotations for n argument

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -6,7 +6,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n, max_delay: int) -> List[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Asynchronous coroutine that spawns wait_random n times with the
     specified max_delay.
